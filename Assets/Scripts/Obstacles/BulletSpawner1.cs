4d4519844b55b2c1b28f3f0a831a16ebdc259620
onszik <popovviktor541@gmail.com>
2025-01-30 22:29:52 +0100

    updated bullet spawning script

--- Assets/Scripts/Obstacles/BulletSpawner1.cs
+++ Assets/Scripts/Obstacles/BulletSpawner1.cs
@@ -1,47 +1,80 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BulletSpawner1 : MonoBehaviour {
-    public GameObject bullet;
+
+    public GameObject bulletPrefab;
+    private Queue<GameObject> bulletPool = new Queue<GameObject>();
+    private List<GameObject> activeBullets = new List<GameObject>();
+
+    #region bullet stats
 
     public float delay = 3f;
-    public float bulletSpeed = 10f;
-
     public float numOfBullets = 4;
     public float numOfBulletsIncrease = 0.5f;
+    public float bulletSpeed = 10f;
 
-    public float flipX = 0;
-    public float flipY = 0;
+    #endregion
+
+    public bool right = true;
+    public bool down = true;
 
     private void Start() {
         InvokeRepeating("SpawnBullets", delay, delay);
     }
 
+    private void FixedUpdate() {
+        MoveBullets();
+    }
+
     void SpawnBullets() {
         for (int i = 0; i < Mathf.Floor(numOfBullets); i++) {
 
-            GameObject b = Instantiate(bullet) as GameObject;
+            GameObject b = GetFromPool();
             b.transform.position = transform.position;
-            var rb = b.GetComponent<Rigidbody2D>();
 
-            float xspd = Random.Range(1, bulletSpeed + 1);
-            float yspd = Mathf.Clamp(bulletSpeed / xspd, 0, bulletSpeed);
+            Vector3 dir = new Vector2(1, -1);
+            if (!right)
+                dir.x = -1;
+            if (!down)
+                dir.y = 1;
 
-            if (flipX > 0) {
-                if (Random.value <= flipX) {
-                    xspd *= -1;
-                }
-            }
-            if (flipY > 0) {
-                if (Random.value <= flipY) {
-                    yspd *= -1;
-                }
-            }
-
-            rb.velocity = new Vector2(xspd - 1, yspd);
-            Destroy(b, 7f);
+            b.transform.right = dir;
+            b.transform.Rotate(0f, 0f, Random.Range(-70f, 70f));
         }
 
-        counter = 0f;
         numOfBullets += numOfBulletsIncrease;
+    }
+
+    GameObject GetFromPool() {
+        if (bulletPool.Count > 0) {
+            GameObject b = bulletPool.Dequeue();
+            b.SetActive(true);
+            activeBullets.Add(b);
+            return b;
+        } else {
+            GameObject b = Instantiate(bulletPrefab);
+            activeBullets.Add(b);
+            return b;
+        }
+    }
+
+    public void PutInPool(GameObject bullet) {
+        bullet.SetActive(false);
+        bulletPool.Enqueue(bullet);
+    }
+
+    private void MoveBullets() {
+        for (int i = activeBullets.Count - 1; i >= 0; i --) {
+            GameObject b = activeBullets[i];
+
+            b.transform.position += (Vector3)b.transform.right * bulletSpeed * Time.deltaTime;
+
+            if (b.transform.position.magnitude > 10f) // adjust for screen size
+            {
+                activeBullets.RemoveAt(i);
+                PutInPool(b);
+            }
+        }
     }
 }
--- Assets/Scripts/Obstacles/LazerSpawner1.cs
+++ Assets/Scripts/Obstacles/LazerSpawner1.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class LazerSpawner1 : MonoBehaviour
 {
-    public GameObject lazer;
+    public GameObject lazerPrefab;
+    private Queue<GameObject> lazerPool = new Queue<GameObject>();
 
     public AudioSource sound;
 
@@ -12,30 +13,63 @@ public class LazerSpawner1 : MonoBehaviour
     public float numOfLazers = 2;
     public float numOfLazersIncrease = 1;
 
-    private float counter = 0f;
-
     private Camera cam;
 
+    private float width;
+
     void Start() {
+        width = (cam.orthographicSize * cam.GetComponent<Camera>().aspect) - 2;
         cam = Camera.main;
+
+        InvokeRepeating("SpawnBullets", delay, delay);
     }
 
-    void FixedUpdate() {
-        counter += Time.fixedDeltaTime;
+    void SpawnLazers() {
+        for (int i = 0; i < Mathf.Floor(numOfLazers); i++) {
+            GameObject l = GetFromPool();
 
-        if (counter >= delay) {
-            for (int i = 0; i < Mathf.Floor(numOfLazers); i++) {
-                GameObject l = Instantiate(lazer);
-
-                float width = (cam.orthographicSize * cam.GetComponent<Camera>().aspect) - 2; 
-
-                l.transform.position = new Vector2(Random.Range(-width, width), 0);
-                l.transform.Rotate(new Vector3(0, 0, Random.Range(0, 359)), Space.World);
-            }
-            numOfLazers += numOfLazersIncrease;
-            counter = 0f;
-            sound.Play();
+            l.transform.position = new Vector2(Random.Range(-width, width), 0);
+            l.transform.Rotate(new Vector3(0, 0, Random.Range(0, 359)), Space.World);
         }
+        numOfLazers += numOfLazersIncrease;
+
+        sound.Play();
+    }
+
+    GameObject GetFromPool() {
+        if (lazerPool.Count > 0) {
+            GameObject b = lazerPool.Dequeue();
+            b.SetActive(true);
+            return b;
+        } else {
+            GameObject b = Instantiate(lazerPrefab);
+            return b;
+        }
+    }
+
+    public void PutInPool(GameObject lazer) {
+        lazer.SetActive(false);
+        lazerPool.Enqueue(lazer);
+    }
+
+    /*
+    IEnumerator AnimateLazers() {
+        float t = 0;
+
+        yield return new WaitForSeconds(1.1f);
+
+        while (t < 1.5f) {
+            t += Time.deltaTime;
+
+            
+        }
+        
+        
+    }
+    */
+
+    public float DamageCheck() {
+
     }
 }
 
